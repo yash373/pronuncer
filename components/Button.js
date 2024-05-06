1bb05aa2c0b2c38a8bdf1d39ea57f89c0c60623f
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 16:12:24 +0530

    fixed display issues

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, action}) => {
   return (
-    <button className='bg-blue-500 mx-auto my-auto p-2 rounded-md w-1/2' onClick={()=>{action()}}>
+    <button className='bg-blue-500 mx-auto my-auto p-2 rounded-md w-full' onClick={()=>{action()}}>
       {text}
     </button>
   )
--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const Index = () => {
       <Title text={"word pronouncer"} />
       <img className='h-[30vh] w-[19%] my-auto mx-auto' src={"https://th.bing.com/th/id/OIP.aXaQwWXMQPa8e2VCSfyYTwHaJ0?w=115&h=180&c=7&r=0&o=5&pid=1.7"} />
       <input onChange={updateText} value={text} placeholder='Enter your text here' className='text-center w-[50%] mx-auto my-auto rounded-md h-[10vh] p-5 bg-slate-100' />
-      <div className='mx-auto my-auto flex flex-col w-[90vh] gap-4'>
+      <div className='mx-auto my-auto flex flex-col h-[10vh] gap-4'>
         <Button text={"Speak in Russian"} action={() => { speakRussian() }} />
         <Button text={"Speak in English"} action={() => { say(text) }} />
       </div>
