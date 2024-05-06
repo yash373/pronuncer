642e35958af58331f0b19d8365dd629390f0ed95
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 16:46:34 +0530

    tried fixing bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Word Pronouncer</title>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <GoogleTagManager gtmId="G-9RLE5VT5MB" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9RLE5VT5MB"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9RLE5VT5MB');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
