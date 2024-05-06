73277079373448a6f84a2530347da00dccd66a4e
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 15:07:20 +0530

    added speach function

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Title from '@/components/Title'
 import { useSpeechSynthesis } from 'react-speech-kit'
 
 const Index = () => {
+  const { speak } = useSpeechSynthesis()
+
+  const say = (text) => {
+    speak({ text })
+  }
+
   return (
     <div className='flex h-[100vh] w-full'>
       <Title text={"word pronouncer"} />
