44c16e2293f8644aec3f38697f3a8a120a492986
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 15:19:31 +0530

    added textbox for input

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ const Index = () => {
     <div className='flex h-[100vh] flex-col w-full'>
       <Title text={"word pronouncer"} />
       <img className='h-[30vh] w-[19%] my-auto mx-auto' src={"https://th.bing.com/th/id/OIP.aXaQwWXMQPa8e2VCSfyYTwHaJ0?w=115&h=180&c=7&r=0&o=5&pid=1.7"} />
+      <input placeholder='Enter your text here'  className='text-center w-[50%] mx-auto my-auto rounded-md h-[10vh] p-5 bg-slate-100' />
     </div>
   )
 }
