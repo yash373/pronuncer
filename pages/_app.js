bf996f410e258fa2a0911b9566a1d9b9c1bfbac3
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 16:25:52 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,15 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Word Pronouncer</title>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9RLE5VT5MB"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9RLE5VT5MB');
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
