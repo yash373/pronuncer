103baac89d52b7d43404d98c2e993ddde254a35e
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 16:04:33 +0530

    added API for translation

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Button from '@/components/Button'
 
 const Index = () => {
   const { speak } = useSpeechSynthesis()
-  const [text,setText] = useState("")
+  const [text, setText] = useState("")
 
   const say = (text) => {
     speak({ text })
@@ -15,14 +15,45 @@ const Index = () => {
     setText(e.target.value)
   }
 
+  const getRussianText = async () => {
+    const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': '2010442decmshc62de9e7247e8e1p15b96cjsn1cf015d3184b',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+      },
+      body: new URLSearchParams({
+        q: text,
+        target: 'ru',
+        source: 'en'
+      })
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      return result["data"]["translations"][0]["translatedText"]
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const speakRussian = async() => {
+    const russianText = await getRussianText()
+    // say(t)
+  }
+
   return (
     <div className='flex h-[100vh] flex-col w-full'>
       <Title text={"word pronouncer"} />
       <img className='h-[30vh] w-[19%] my-auto mx-auto' src={"https://th.bing.com/th/id/OIP.aXaQwWXMQPa8e2VCSfyYTwHaJ0?w=115&h=180&c=7&r=0&o=5&pid=1.7"} />
-      <input onChange={updateText} value={text} placeholder='Enter your text here'  className='text-center w-[50%] mx-auto my-auto rounded-md h-[10vh] p-5 bg-slate-100' />
+      <input onChange={updateText} value={text} placeholder='Enter your text here' className='text-center w-[50%] mx-auto my-auto rounded-md h-[10vh] p-5 bg-slate-100' />
       <div className='mx-auto my-auto flex flex-col w-[90vh] gap-4'>
-        <Button text={"Speak in Russian"} />
-        <Button text={"Speak in English"} action={()=>{say(text)}} />
+        <Button text={"Speak in Russian"} action={() => { speakRussian() }} />
+        <Button text={"Speak in English"} action={() => { say(text) }} />
       </div>
     </div>
   )
