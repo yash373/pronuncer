a74af27c709467d4969c8eca2719786a1c9364a6
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 15:02:07 +0530

    Gave page a title

---
+++ components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({text}) => {
+  return (
+    <div className='text-3xl capitalize flex font-semibold mx-auto my-auto'>
+      {text}
+    </div>
+  )
+}
+
+export default Title
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Title from '@/components/Title'
 
 const Index = () => {
   return (
-    <div>
-      
+    <div className='flex h-[100vh] w-full'>
+      <Title text={"word pronouncer"} />
     </div>
   )
 }
