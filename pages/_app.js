fd8af0217930d360b64f8d94cbed9235881a04af
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-05-06 16:34:12 +0530

    bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,14 +9,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Word Pronouncer</title>
         {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9RLE5VT5MB"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-9RLE5VT5MB');
-        </script>
+        <GoogleAnalytics gaId="G-9RLE5VT5MB" />
       </Head>
       <Component {...pageProps} />
     </>
